f47f6e17b193d460e164e8696c7302f4e36bf161
Dave Solares <dasolare@microsoft.com>
2021-05-28 13:55:14 -0700

    test

--- GLTFSDK/Source/SchemaValidation.cpp
+++ GLTFSDK/Source/SchemaValidation.cpp
@@ -34,7 +34,7 @@ namespace
                 throw GLTFException("Schema document at " + uri + " is not valid JSON");
             }
 
-            auto result = schemaDocuments.emplace(uri, rapidjson::SchemaDocument(document));
+            auto result = schemaDocuments.emplace(uri, rapidjson::SchemaDocument(document, this, nullptr));
             assert(result.second);
             auto resultSchemaDoc = &(result.first->second);
             assert(resultSchemaDoc);
