eaccf166e2718c6133db426545b6d008cb7ad79f
Sergio R. Z. Masson <97050577+SergioRZMasson@users.noreply.github.com>
2022-06-27 19:19:00 -0300

    Bugfix/update rapid json (#94)
    Updated rapidjson version to latest.
    
    Updated CI and removed legacy Visual Studio 2017 integration tests (No longer supported by Azure)
    
    Temporarily disabled vcpkg integration tests since we are currently incompatible with its version of rapidjson. Once vcpkg updates its rapid-json we should we enable vcpkg integration tests. 
    
    Fixes #93

--- GLTFSDK/Source/ExtensionsKHR.cpp
+++ GLTFSDK/Source/ExtensionsKHR.cpp
@@ -234,7 +234,7 @@ std::unique_ptr<Extension> KHR::Materials::DeserializePBRSpecGloss(const std::st
     Materials::PBRSpecularGlossiness specGloss;
 
     auto doc = RapidJsonUtils::CreateDocumentFromString(json);
-    const rapidjson::Value sit = doc.GetObject();
+    const auto sit = doc.GetObject();
 
     // Diffuse Factor
     auto diffuseFactIt = sit.FindMember("diffuseFactor");
@@ -314,7 +314,7 @@ std::unique_ptr<Extension> KHR::Materials::DeserializeUnlit(const std::string& j
     Unlit unlit;
 
     auto doc = RapidJsonUtils::CreateDocumentFromString(json);
-    const rapidjson::Value objValue = doc.GetObject();
+    const auto objValue = doc.GetObject();
 
     ParseProperty(objValue, unlit, extensionDeserializer);
 
@@ -375,7 +375,7 @@ std::unique_ptr<Extension> KHR::MeshPrimitives::DeserializeDracoMeshCompression(
     auto extension = std::make_unique<DracoMeshCompression>();
 
     auto doc = RapidJsonUtils::CreateDocumentFromString(json);
-    const rapidjson::Value v = doc.GetObject();
+    const auto v = doc.GetObject();
 
     extension->bufferViewId = GetMemberValueAsString<uint32_t>(v, "bufferView");
 
@@ -481,7 +481,7 @@ std::unique_ptr<Extension> KHR::TextureInfos::DeserializeTextureTransform(const 
     TextureTransform textureTransform;
 
     auto doc = RapidJsonUtils::CreateDocumentFromString(json);
-    const rapidjson::Value sit = doc.GetObject();
+    const auto sit = doc.GetObject();
 
     // Offset
     auto offsetIt = sit.FindMember("offset");
